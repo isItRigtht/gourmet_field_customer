1351a7c492cbd7fff36efd24a9aca32b278cdaae
UESTC-Majialu <1835027607@qq.com>
2024-08-24 11:22:01 +0800

    首页主体结构完成

--- src/pages/field/field.vue
+++ src/pages/field/field.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
+import SortGoods from '@/components/SortGoods.vue'
 </script>
 
 <template>
   field
+  <!-- 测试商品排序 -->
+  <SortGoods />
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
